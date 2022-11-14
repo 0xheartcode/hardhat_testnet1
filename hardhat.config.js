d56ae59c78008240379da64d0318f601d09224f4
0xheartcode <0xheartcode@gmail.com>
2022-11-14 07:20:06 +0100

    hardhatclean

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+// Import confidential .env files
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { INFURA_API_KEY, MNEMONIC, snowtraceApiKey } = process.env;
+
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    fuji: {
+	    url: INFURA_API_KEY,
+	    chainId: 43113,
+	    accounts: { mnemonic: MNEMONIC },     
+    }
+  },
+	etherscan: {
+		apiKey: snowtraceApiKey,
+	},
 };
